9ef9760daaa14b22a206e86c1f2bbfae0ad6ee0f
rno9i <iirn8944@gmail.com>
2025-05-06 13:28:43 +0300

    done

---
+++ main4.js
@@ -0,0 +1,29 @@
+let saudiaAirlines ={
+    airlineName:"الخطوط الجوية السعودية",
+    country:"السعودية",
+    fleetSize:200,
+    destenations:["نيويورك","جدة","دبي","الرياض"],
+    inOperation:true,
+    bookFlight:function(destenation){
+        if(this.destenations.includes(destenation)){
+            console.log(`تم حجز رحلة إلى ${destenation}`)
+        }else{
+            console.log(`عذرا لايوجد رحلات إلى ${destenation}حاليا`)
+        }
+    },
+    addDestination: function(addnewDes) {
+        if (!this.destenations.includes(addnewDes)) {
+            this.destenations.push(addnewDes);
+            console.log(`${addnewDes} تم إضافة الوجهة بنجاح.`);
+        } else {
+            console.log("unknown");
+        }
+    },
+    getSummary:()=>{
+console.log("الخطوط الجوية السعودية تعمل في السعودية وتمتلك 120 طائرة وتطير إلى 20 وحهة")
+    }
+}
+saudiaAirlines.getSummary();
+
+saudiaAirlines.bookFlight("الرياض");
+saudiaAirlines.addDestination("أبها")
